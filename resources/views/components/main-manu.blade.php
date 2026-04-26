481dd78a9c36faa61b662ea4c04ffeaf317f6f63
Wazihatulla <08wasti@gmail.com>
2026-04-27 01:51:26 +0600

    Dainamically Catagory List Show with axios

--- resources/views/components/main-manu.blade.php
+++ resources/views/components/main-manu.blade.php
@@ -22,77 +22,10 @@
                         <div class="container bg-white border mx-auto w-lg-75 p-3 border rounded-lg-0 rounded responsve-dropdown">
                             <div class="row gy-lg-0 gy-3">
                             <!-- === Property Page Slider Design === -->
-                            <div class="col-lg-3 col-sm-6">
-                                <p class="text-uppercase fw-bolder opacity-75 mb-2">
-                                    <small>property pasge slider design</small>
-                                </p>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/spacious-office-space.php">
-                                    <small>Slider Vertical</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/slider-horizontal.php">
-                                    <small>Slider Horizontal</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/slider-gallery.php">
-                                    <small>Slider Gallery</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/full-width.php">
-                                    <small>Full Width</small>
-                                </a>
+                            <div class="col-lg-3 col-sm-6" id="CategoryItem">
+                                
                             </div>
-                            <!-- === Property page header type === -->
-                            <div class="col-lg-3 col-sm-6">
-                                <p class="text-uppercase fw-bolder opacity-75 mb-2">
-                                    <small>Property page header type</small>
-                                </p>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/house-with-swimming-pool.php">
-                                    <small>No Header</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/villa-to-be-refurnished.php">
-                                    <small>Google Map</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/house-with-garden-and-pool.php">
-                                    <small>Revolution Slider</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/unique-house-in-miami.php">
-                                    <small>Theme Slider Type 2</small>
-                                </a>
-                            </div>
-                            <!-- === Properties Lists === -->
-                            <div class="col-lg-3 col-sm-6">
-                                <p class="text-uppercase fw-bolder opacity-75 mb-2">
-                                    <small>Properties Lists</small>
-                                </p>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/property-list-half-map.php">
-                                    <small>Half Map</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/properties-list.php">
-                                    <small>Standard – No Sidebar</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/property-list-with-sidebar.php">
-                                    <small>Standard – Sidebar Right</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/property-list-sidebar-left.php">
-                                    <small>Standard – Sidebar Left</small>
-                                </a>
-                            </div>
-                            <!-- === Properties by Type === -->
-                            <div class="col-lg-3 col-sm-6">
-                                <p class="text-uppercase fw-bolder opacity-75 mb-2">
-                                    <small>Properties by Type</small>
-                                </p>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/apartments.php">
-                                    <small>Category</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/sales.php">
-                                    <small>Type</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/miami.php">
-                                    <small>City</small>
-                                </a>
-                                <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold " href="http://localhost/Real-Estate-FrontEnd/south-beach.php">
-                                    <small>Area</small>
-                                </a>
-                            </div>
+                           
                             </div>
                         </div>
                     </div>	
@@ -123,3 +56,37 @@
         </div>
     </div>
 </nav>
+<script>
+    Category();
+
+    async function Category() {
+        let res = await axios.get("/CategoryList");
+
+        let data = res.data.data;
+
+        let perColumn = 10;
+        let container = $('#CategoryItem').parent();
+
+        $('#CategoryItem').remove();
+
+        for (let i = 0; i < data.length; i += perColumn) {
+
+            let columnItems = data.slice(i, i + perColumn);
+
+            let column = `
+                <div class="col-lg-2 col-sm-6">
+                    <p class="text-uppercase fw-bolder opacity-75 mb-2">
+                        <small>Category</small>
+                    </p>
+                    ${columnItems.map(item => `
+                        <a class="ps-0 d-block pb-sm-2 pb-1 text-decoration-none text-black-50 text-goldren-300-hover fw-semibold" href="#">
+                            <small>${item.categoryName}</small>
+                        </a>
+                    `).join('')}
+                </div>
+            `;
+
+            container.append(column);
+        }
+    }
+</script>
