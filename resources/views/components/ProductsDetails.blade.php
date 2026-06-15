8440d6c5d3a2d52cb714913c84d8977cc676e617
Wazihatulla <08wasti@gmail.com>
2026-06-15 20:12:05 +0600

    add prodcts details title, descreption, add to cart , size and wishes

--- resources/views/components/ProductsDetails.blade.php
+++ resources/views/components/ProductsDetails.blade.php
@@ -25,7 +25,41 @@
           </div>
           <!-- product details -->
           <div class="col-md-6">
-            
+             <!-- product title and price -->
+             <div class="mb-4">
+                <h1 id="pr_title" class="mb-2">Title</h1>
+                <h4 id="pr_price">Price</h4>
+             </div>
+             <p id="pr_des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum illo suscipit nostrum sapiente quasi adipisci voluptatum vitae molestiae facere. Tenetur omnis iste, voluptatem est officia doloremque quod qui ad voluptatibus!</p>
+             <!-- size -->
+             <label for="size">Size</label>
+             <select name="size" id="size" class="form-control"></select>
+             <!-- color -->
+             <label for="color">Color</label>
+             <select name="color" id="color" class="form-control"></select>
+             <!-- quantity -->
+             <div class="d-flex flex-wrap align-items-center gap-3 mt-4">
+
+                <!-- Quantity -->
+                <div class="input-group" style="width: 140px;">
+                    <button class="btn btn-outline-secondary minus" type="button">-</button>
+
+                    <input type="text" class="form-control text-center" value="1">
+
+                    <button class="btn btn-outline-secondary plus" type="button">+</button>
+                </div>
+
+                <!-- Add to Cart -->
+                <button class="btn btn-danger px-4">
+                    <i class="bi bi-cart-fill me-2"></i>
+                    Add to Cart
+                </button>
+
+                <!-- Wishlist -->
+                <button class="btn btn-outline-danger">❤️</button>
+
+             </div>
+
           </div>
        </div> 
     </div>
